cf3fa1ec681b416d8cdbe9583b0830e58841529a
Jon Minder <info@ayalon.ch>
2021-11-09 13:20:33 +0100

    Make unbuild work with type declaration.

--- build.config.ts
+++ build.config.ts
@@ -1,13 +1,21 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  // declaration: true,
+  declaration: true,
   emitCJS: false,
   cjsBridge: true,
-  entries: ['src/module', 'src/templates/plugin'],
+  //entries: ['src/module', 'src/templates/plugin'],
+  entries: [
+    //'src/templates/plugin',
+    { input: 'src/module', format: 'esm', builder: 'rollup' },
+  ],
+  inlineDependencies: false,
   externals: [
-    '@types/express-serve-static-core',
+    '@graphql-codegen/cli',
     '@nuxt/kit-edge',
     '@nuxt/types',
+    '@types/express-serve-static-core',
+    'express',
   ],
+  dependencies: ['esbuild', 'typescript', 'express'],
 })
