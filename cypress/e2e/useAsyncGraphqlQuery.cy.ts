4435a68f98baceea4c54e35f7ed0da384df3e1ec
Jan Hug <jan.hug@liip.ch>
2025-06-08 10:56:13 +0200

    add wait in cypress test

--- cypress/e2e/useAsyncGraphqlQuery.cy.ts
+++ cypress/e2e/useAsyncGraphqlQuery.cy.ts
@@ -7,11 +7,13 @@ describe('useAsyncGraphqlQuery', () => {
     cy.get('#normal-value').should('have.text', '0')
 
     cy.get('#increment').click()
+    cy.wait(700)
     cy.get('#value').should('have.text', '1')
     cy.get('#wrapped-value').should('have.text', '1')
     cy.get('#normal-value').should('have.text', '1')
 
     cy.get('#refresh').click()
+    cy.wait(700)
     // Value should remain the same.
     cy.get('#value').should('have.text', '1')
     cy.get('#wrapped-value').should('have.text', '1')
